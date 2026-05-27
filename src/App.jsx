80c6354551b6dbb669bc85ddbd7756188a9fc283
FiryanSyah <firyansyah22@gmail.com>
2026-05-27 18:53:13 +0700

    update links to match the router & move Orders Page into react

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
 import CheckoutPage from "./pages/checkout/CheckOut";
+import OrderPages from "./pages/orders/orders";
 function App() {
 	return (
 		<Routes>
 			<Route index element={<HomePage />} />
 			<Route path="checkout" element={<CheckoutPage />} />
+			<Route path="orders" element={<OrderPages />} />
 		</Routes>
 	);
 }
